185793a8c47ca0d0762039aa8fc3a2523a50f842
AmdSadi <36379723+AmdSadi@users.noreply.github.com>
2018-04-16 10:52:08 +0600

    Add files via upload

---
+++ New/CSSexample.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<head>
+		<style>
+		#container{
+					background-color: #0099ff;
+					height:300px;
+					width: 600px;
+					border: 25px solid white;
+					padding: 25px;
+					margin-top: 225px;
+					margin-left: 350px;
+				}
+		#sec{
+			background-color:white;
+			width:500px;
+			height:50px ;
+			margin-top:20px; 
+			margin-left:40px;
+			
+		}
+		#sec2{
+			
+			border: 5px solid black;
+			height:300px;
+			width:482px;
+			
+		}
+			</style>
+</head>
+	<body style="background-color:black;">
+		<div id="container">
+				<div id="sec" >
+				<h1 align="center">CSC 3222: Web Technologies</h1>
+				</div>
+				<div id="sec" style="padding: 10px;border: 5px solid black;height:180px;width:482px;">
+				<p>
+				master builder of human happiness.No one rejects,dislikes or avoids pleasure itself.Because it is pleasure,
+				but because those who do not know how to pursue pleasure rationally encounter consequences
+				</p> 
+				</div>
+		</div>
+	</body>
+</html>
